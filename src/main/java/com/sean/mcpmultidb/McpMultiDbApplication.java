e694d7c8f7046b49dc6f946c18304b25fffc710a
dengxingquan <dengxingquan@xgd.com>
2026-03-09 17:18:05 +0800

    Initial commit

---
+++ src/main/java/com/sean/mcpmultidb/McpMultiDbApplication.java
@@ -0,0 +1,13 @@
+package com.sean.mcpmultidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpMultiDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpMultiDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sean/mcpmultidb/McpMultiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean.mcpmultidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpMultiDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
